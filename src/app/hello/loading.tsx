00e2596b3898ffdf5f26cc74d379cb117135d4e0
EndYou01 <jorgesenjudo@gmail.com>
2023-06-16 10:53:34 -0400

    Static Rendering in Server Components

--- src/app/hello/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { Spinner } from "@/components/bootstrap";
-
-
-export default function Loading() {
-    return <Spinner animation="border" className="d-block m-auto" />
-}
---
+++ src/models/unsplash-image.ts
@@ -0,0 +1,11 @@
+export interface UnslpashImage {
+    description: string,
+    user: {
+        username: string
+    },
+    urls: {
+        raw: string
+    },
+    width: number,
+    height: number,
+}
